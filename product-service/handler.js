4bed52f6101deca2eb2f7af707c162addd375c6f
bdvx <Denis_Beskorovainyi>
2022-07-24 21:26:44 +0400

    task 4

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,7 +1,9 @@
 import { getProductById } from './src/getProductById.js';
 import { getAllProducts } from './src/getAllProducts.js';
+import { createProduct } from './src/createProduct.js';
 
 export {
   getProductById,
-  getAllProducts
+  getAllProducts,
+  createProduct,
 }
