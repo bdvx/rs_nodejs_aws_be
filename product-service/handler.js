b78e05af739f2b5eeedd3d6c831dabf7718ff06e
bdvx <bdv2507@gmail.com>
2022-07-24 20:36:39 +0400

    Task3 (#1)
    * draft endpoints
    
    * updated serverless package
    
    Co-authored-by: Denis Beskorovainyi <Denis_Beskorovainyi@epam.com>
    Co-authored-by: bdvx <Denis_Beskorovainyi>

---
+++ product-service/handler.js
@@ -0,0 +1,7 @@
+import { getProductById } from './src/getProductById.js';
+import { getAllProducts } from './src/getAllProducts.js';
+
+export {
+  getProductById,
+  getAllProducts
+}
---
+++ product-service/src/productList.js
@@ -0,0 +1,71 @@
+// mocked image
+const URL = "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80";
+
+const productList = [
+    {
+      "count": 4,
+      "description": "Full-sized headphones",
+      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+      "price": 29990,
+      "title": "Sony WH-1000XM4 Black",
+      "image": URL
+    },
+    {
+      "count": 6,
+      "description": "Full-sized headphones",
+      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
+      "price": 29990,
+      "title": "Bowers & Wilkins PX7 Space Gray",
+      "image": URL
+    },
+    {
+      "count": 7,
+      "description": "Full-sized headphones",
+      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
+      "price": 26990,
+      "title": "Sennheiser Momentum 3 Wireless M3AEBTXL Black",
+      "image": URL
+    },
+    {
+      "count": 12,
+      "description": "Full-sized headphones",
+      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+      "price": 159990,
+      "title": "Sennheiser HD820",
+      "image": URL
+    },
+    {
+      "count": 7,
+      "description": "Full-sized headphones",
+      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
+      "price": 14990,
+      "title": "Audio-Technica ATH-M50xBT",
+      "image": URL
+    },
+    {
+      "count": 8,
+      "description": "True wireless earphones",
+      "id": "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+      "price": 14990,
+      "title": "Sony WF-1000XM3 Black",
+      "image": URL
+    },
+    {
+      "count": 2,
+      "description": "True wireless earphones",
+      "id": "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+      "price": 22890,
+      "title": "Sennheiser Momentum True Wireless 2 Black",
+      "image": URL
+    },
+    {
+      "count": 3,
+      "description": "True wireless earphones",
+      "id": "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+      "price": 12790,
+      "title": "Audio-Technica ATH-CKR7TW BK",
+      "image": URL
+    }
+  ]
+
+  export default productList;
