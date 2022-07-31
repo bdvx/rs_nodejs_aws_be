edc45f918985698b80c70e89cf5fff4d1190c257
bdvx <bdv2507@gmail.com>
2022-08-01 01:54:07 +0400

    task 4 (#2)

--- product-service/src/getAllProducts.js
+++ product-service/src/getAllProducts.js
@@ -1,16 +1,44 @@
-import productList from './productList.js';
+import { Client } from 'pg';
+import { headers, errMessage } from './helpers.js';
+import { DB_OPTIONS } from './constants.js';
+import { URL } from './productList.js';
 
 export const getAllProducts = async (event) => {
   console.log('getAllProducts lambda called with event: ', event);
   
-  // emulate delay from remote URL fetch
-  // suppose we'll get real URL here and not file with hardcoded values
-  await new Promise(resolve => setTimeout(resolve, 2000));
+  // get new DB client (cause using pools is not good for lambdas)
+  const client = new Client(DB_OPTIONS);
+  await client.connect();
+
+  try {
+
+  // search needed product - by id field
+  let { rows } = await client.query(`
+  select * from products p inner join stocks s on p.id = s.product_id
+  `);
+
+    // string just to add image mock, will be removed
+    rows = rows.map((val)=>{val.image = URL; return val;});
+
+  if (!rows){
+    return {
+        headers,
+        statusCode: 404,
+        body: "Target products were not found",
+      };
+  }
 
   return {
-    headers: { "Access-Control-Allow-Origin": "*" },
+    headers,
     statusCode: 200,
-    body: JSON.stringify(productList)
+    body: JSON.stringify(rows),
   };
+
+  } catch (err) {
+    console.error('getProductById lambda crashed with error:', err)
+    return errMessage;
+  } finally {
+    client.end();
+  }
 
 };
